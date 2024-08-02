b037460b0b5201d7890a8b53c0cfcfbed36d51a2
aallen417 <austin.allen417@gmail.com>
2024-08-02 19:42:20 -0400

    implements show functionality

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,14 +2,12 @@ import { Skill } from "../models/skill.js"
 
 async function index(req, res) {
   try {
-    const skills = await Skill.find({})
-  
-  
+    const skills = await Skill.find({})  
     res.render("skills/index", {
       skills
-    })
-    
+    })    
   } catch (error) {
+    console.log(error)    
     res.redirect(("/"))
   }
 }
@@ -19,13 +17,31 @@ async function newSkill(req, res) {
 }
 
 async function create(req, res) {
-  req.body.learned = false
-  await Skill.create(req.body)
-  res.redirect("/skills")
+  try {
+    req.body.learned = false
+    await Skill.create(req.body)
+    res.redirect("/skills")    
+  } catch (error) {
+    console.log(error)    
+    res.redirect("/skills")
+  }
+}
+
+async function show(req, res) {
+  try {
+    const skill = await Skill.findById(req.params.skillId)
+    res.render("skills/show", {
+      skill
+    })    
+  } catch (error) {
+    console.log(error)    
+    res.redirect("/skills")
+  }
 }
 
 export {
   index,
   newSkill as new,
   create,
+  show,
 }
