76af972e766a423e09d9f760cfb7b963a97c62af
aallen417 <austin.allen417@gmail.com>
2024-08-02 14:56:46 -0400

    adds skills route, controller, and skills index view

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {text: "HTML", learned: true, _id: 125223},
+  {text: "CSS", learned: true, _id: 127904},
+  {text: "Javascript", learned: true, _id: 139608},
+]
+
+export {
+  skills
+}
